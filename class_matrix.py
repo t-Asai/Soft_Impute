d06544e0135d55a8883227fd36ac618a1e92b59f
t-Asai <takumi.a42@gmail.com>
2018-02-18 11:05:41 +0900

    add matrix class and adjust methods

---
+++ class_matrix.py
@@ -0,0 +1,60 @@
+import numpy as np
+import random
+
+
+class Matrix:
+
+    def __init__(self, N=100, M=100, rate_dense=0.1, rate_sample=0.7, rate_train_test=0.3):
+        self.N = N
+        self.M = M
+        self.rate_dense = rate_dense
+        self.rate_sample = rate_sample
+        self.rate_train_test = rate_train_test
+
+    def make_target_matrix(self):
+        """
+        アルゴリズムが正しいかどうかを調べるために、低ランクな行列を作成する関数
+        """
+        X = np.random.random((self.N, self.M))
+        U, S, V = np.linalg.svd(X, full_matrices=False)
+        S_ = np.array([s if i < self.rate_dense *
+                       self.N else 0 for i, s in enumerate(S)])
+        X = np.dot(np.dot(U, np.diag(S_)), V)
+        self.Original = X
+
+    def split_to_test_and_train(self):
+        """
+        再構成がうまく出来ているかどうかを調べるために、訓練データとテストデータに分ける
+        0の値は0を観測したのか観測できなかったのかの判断が面倒なので、
+        この実装では0は観測できなかったとみなす。
+        よって、比較するためには非ゼロの値を分離して比較する必要がある。
+        """
+        List = []
+        for i in range(self.N):
+            for j in range(self.M):
+                if self.Original[i, j] != 0:
+                    List.append([i, j])
+        random.shuffle(List)
+
+        num_train = len(List) * self.rate_train_test
+        self.Train = np.zeros((self.N, self.M))
+        self.Test = np.zeros((self.N, self.M))
+        for e, (i, j) in enumerate(List):
+            if e < num_train:
+                self.Train[i, j] = self.Original[i, j]
+            else:
+                self.Test[i, j] = self.Original[i, j]
+
+    def make_sampling_matrix(self):
+        """
+        観測行列を作成する関数
+        テスト用にとっておいた部分は、観測できなかったことにする
+        rateの意味は、本来観測できたであろう部分から、もっと差っ引きたかったら1より小さくすればいい
+        """
+        self.Cast = np.zeros((self.N, self.M))
+        for i in range(self.N):
+            for j in range(self.M):
+                if self.Test[i, j] == 0 and random.random() < self.rate_sample:
+                    self.Cast[i, j] = 1.0
+        self.Observe = self.Cast * self.Train
+        # self.Observe += 0.01 * np.random.normal(0, 1, (self.N, self.M))
--- main.py
+++ main.py
@@ -1,45 +1,41 @@
 import pandas as pd
 from collections import namedtuple
-import make_matrix
 from methods_algorithm import warm_start
-from methods_matrix import to_low_rank_matrix
 from methods_graph import plot_val
-
+from class_matrix import Matrix
 
 if __name__ == "__main__":
     """
     メインの関数
     長々と書いてるのをもっと切り分けたい
     """
-    N = 100
+    Mat = Matrix(N=100, M=100, rate_dense=0.1,
+                 rate_sample=0.9, rate_train_test=0.9)
 
     LAMBDA = namedtuple('LAMBDA', ('start', 'end', 'ratio'))
     Lambda_param = LAMBDA(start=pow(10, 2), end=pow(10, -5), ratio=0.9)
-    rho = 0.1
-    under_sampling_rate = 0.9
-    test_train_ratio = 0.8
 
     stop_condition = pow(10, -1)
 
     """
     初期値作成
     """
-    X0 = make_matrix.make_target_matrix(N, rho)
-    # X0 = to_square_matrix(X0)
-    X0 = to_low_rank_matrix(X0, rho)
-    # X0 = pd.read_csv('data.csv')
+    Mat.make_target_matrix()
+    Mat.split_to_test_and_train()
+    Mat.make_sampling_matrix()
 
     """
     評価用にデータを分ける
     """
-    X_train, X_test = make_matrix.split_to_test_and_train(X0, test_train_ratio)
+    X_train = Mat.Train
+    X_test = Mat.Test
+    Y = Mat.Observe
+    R = Mat.Cast
 
     """
     再構成時に使用するデータをアンダーサンプリングする
     評価用に取り分けた部分は別枠で取り除くので、学習用のデータをさらに差っ引く場合にレートを下げる
     """
-    Y, R = make_matrix.make_sampling_matrix(
-        X_test, X_train, under_sampling_rate)
 
     """
     アルゴリズムを実行する
@@ -49,6 +45,8 @@ if __name__ == "__main__":
     """
     描画する
     """
+    """
     plot_val('cal_total_error')
     plot_val('cal_test_error')
     plot_val('cal_terminal_condition')
+    """
